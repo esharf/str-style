da26de6b8371255be3d31d319a2810476d7393f6
esharf <57587340+esharf@users.noreply.github.com>
2023-05-28 18:06:25 +0300

    improved performence

--- strstyle/str_style.py
+++ strstyle/str_style.py
@@ -1,82 +1,78 @@
-def _paint(color_code: str, str_body: str) -> str:
-    return f'\033[{color_code}m{str_body}\033[0m'
-
-
 def black(str_body: str) -> str:
-    return _paint('30', str_body)
+    return f'\033[30m{str_body}\033[0m'
 
 
 def red(str_body: str) -> str:
-    return _paint('31', str_body)
+    return f'\033[31m{str_body}\033[0m'
 
 
 def green(str_body: str) -> str:
-    return _paint('32', str_body)
+    return f'\033[32m{str_body}\033[0m'
 
 
 def orange(str_body: str) -> str:
-    return _paint('33', str_body)
+    return f'\033[33m{str_body}\033[0m'
 
 
 def blue(str_body: str) -> str:
-    return _paint('34', str_body)
+    return f'\033[34m{str_body}\033[0m'
 
 
 def purple(str_body: str) -> str:
-    return _paint('35', str_body)
+    return f'\033[35m{str_body}\033[0m'
 
 
 def cyan(str_body: str) -> str:
-    return _paint('36', str_body)
+    return f'\033[36m{str_body}\033[0m'
 
 
 def lightgrey(str_body: str) -> str:
-    return _paint('37', str_body)
+    return f'\033[37m{str_body}\033[0m'
 
 
 def darkgrey(str_body: str) -> str:
-    return _paint('90', str_body)
+    return f'\033[90m{str_body}\033[0m'
 
 
 def lightred(str_body: str) -> str:
-    return _paint('91', str_body)
+    return f'\033[91m{str_body}\033[0m'
 
 
 def yellow(str_body: str) -> str:
-    return _paint('93', str_body)
+    return f'\033[93m{str_body}\033[0m'
 
 
 def lightblue(str_body: str) -> str:
-    return _paint('94', str_body)
+    return f'\033[94m{str_body}\033[0m'
 
 
 def pink(str_body: str) -> str:
-    return _paint('95', str_body)
+    return f'\033[95m{str_body}\033[0m'
 
 
 def lightcyan(str_body: str) -> str:
-    return _paint('96', str_body)
+    return f'\033[96m{str_body}\033[0m'
 
 
 def bold(str_body: str) -> str:
-    return _paint('01', str_body)
+    return f'\033[01m{str_body}\033[0m'
 
 
 def disabled(str_body: str) -> str:
-    return _paint('02', str_body)
+    return f'\033[02m{str_body}\033[0m'
 
 
 def underline(str_body: str) -> str:
-    return _paint('04', str_body)
+    return f'\033[04m{str_body}\033[0m'
 
 
 def reverse(str_body: str) -> str:
-    return _paint('07', str_body)
+    return f'\033[07m{str_body}\033[0m'
 
 
 def invisible(str_body: str) -> str:
-    return _paint('08', str_body)
+    return f'\033[08m{str_body}\033[0m'
 
 
 def strikethrough(str_body: str) -> str:
-    return _paint('09', str_body)
+    return f'\033[09m{str_body}\033[0m'
