f581e07709fbbc38a72e56a7a9054ccab0b9f4f2
esharf <57587340+esharf@users.noreply.github.com>
2023-05-28 23:03:42 +0300

    added styles

--- strstyle/str_style.py
+++ strstyle/str_style.py
@@ -1,3 +1,35 @@
+def bold(str_body: str) -> str:
+    return f'\033[01m{str_body}\033[0m'
+
+
+def disabled(str_body: str) -> str:
+    return f'\033[02m{str_body}\033[0m'
+
+
+def italic(str_body: str) -> str:
+    return f'\033[03m{str_body}\033[0m'
+
+
+def underline(str_body: str) -> str:
+    return f'\033[04m{str_body}\033[0m'
+
+
+def sharp(str_body: str) -> str:
+    return f'\033[07m{str_body}\033[0m'
+
+
+def invisible(str_body: str) -> str:
+    return f'\033[08m{str_body}\033[0m'
+
+
+def strikethrough(str_body: str) -> str:
+    return f'\033[09m{str_body}\033[0m'
+
+
+def double_underline(str_body: str) -> str:
+    return f'\033[21m{str_body}\033[0m'
+
+
 def black(str_body: str) -> str:
     return f'\033[30m{str_body}\033[0m'
 
@@ -10,7 +42,7 @@ def green(str_body: str) -> str:
     return f'\033[32m{str_body}\033[0m'
 
 
-def orange(str_body: str) -> str:
+def yellow(str_body: str) -> str:
     return f'\033[33m{str_body}\033[0m'
 
 
@@ -28,6 +60,34 @@ def cyan(str_body: str) -> str:
 
 def lightgrey(str_body: str) -> str:
     return f'\033[37m{str_body}\033[0m'
+
+
+def red_background(str_body: str) -> str:
+    return f'\033[41m{str_body}\033[0m'
+
+
+def green_background(str_body: str) -> str:
+    return f'\033[42m{str_body}\033[0m'
+
+
+def yellow_background(str_body: str) -> str:
+    return f'\033[43m{str_body}\033[0m'
+
+
+def blue_background(str_body: str) -> str:
+    return f'\033[44m{str_body}\033[0m'
+
+
+def purple_background(str_body: str) -> str:
+    return f'\033[45m{str_body}\033[0m'
+
+
+def cyan_background(str_body: str) -> str:
+    return f'\033[46m{str_body}\033[0m'
+
+
+def lightgrey_background(str_body: str) -> str:
+    return f'\033[47m{str_body}\033[0m'
 
 
 def darkgrey(str_body: str) -> str:
@@ -52,27 +112,3 @@ def pink(str_body: str) -> str:
 
 def lightcyan(str_body: str) -> str:
     return f'\033[96m{str_body}\033[0m'
-
-
-def bold(str_body: str) -> str:
-    return f'\033[01m{str_body}\033[0m'
-
-
-def disabled(str_body: str) -> str:
-    return f'\033[02m{str_body}\033[0m'
-
-
-def underline(str_body: str) -> str:
-    return f'\033[04m{str_body}\033[0m'
-
-
-def reverse(str_body: str) -> str:
-    return f'\033[07m{str_body}\033[0m'
-
-
-def invisible(str_body: str) -> str:
-    return f'\033[08m{str_body}\033[0m'
-
-
-def strikethrough(str_body: str) -> str:
-    return f'\033[09m{str_body}\033[0m'
